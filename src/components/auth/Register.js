09b6dcf4f69c558fc75bc9c02c7850007d8ec98c
rachwongrw <rwrachelwong@gmail.com>
2021-03-04 09:37:02 -0800

    edit form style

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -6,7 +6,7 @@ export default function Register() {
   const classes = useStyles();
   const [password, setPW] = useState("")
   const [email, setEmail] = useState("")
-
+  const [username, setUsername] = useState("")
   const submitForm = async (e) => {
     e.preventDefault()
 
@@ -16,8 +16,9 @@ export default function Register() {
       <div className={classes.root}>
         <Typography variant="h6" style={{color: "white"}}>Register</Typography>
         <form onSubmit={submitForm}>
-          <TextField label="Email" variant="filled" onChange={e => setEmail(e.target.value)} className={classes.field}/>
-          <TextField label="Password" onChange={e => setPW(e.target.value)} type="password" variant="filled" className={classes.field}/>
+          <TextField label="Username"  onChange={e => setUsername(e.target.value)} className={classes.field} color="secondary"/>
+          <TextField label="Email" onChange={e => setEmail(e.target.value)} className={classes.field} color="secondary"/>
+          <TextField label="Password" onChange={e => setPW(e.target.value)} type="password" className={classes.field} color="secondary"/>
           <Button type="submit" style={{width: "40%", backgroundColor: "pink"}}>Submit</Button>
         </form>
       </div>
@@ -30,10 +31,21 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 400,
     textAlign: "center",
     display: "block",
-    margin: "0 auto"
+    margin: "0 auto",
+    color: "white"
   },
   field: {
     width: "100%",
-    marginBottom: "1em"
+    marginBottom: "1em",
+    '& label': {
+      color: "white"
+    },
+    '& .MuiInput-underline:before': {
+      borderBottomColor: "white",
+      borderBottom: "1px solid #b9b9b9"
+    },
+    // '& .MuiFilledInput-underline:before': {
+    //   borderBottom: "1px solid #b9b9b9"
+    // }
   }
 }));
